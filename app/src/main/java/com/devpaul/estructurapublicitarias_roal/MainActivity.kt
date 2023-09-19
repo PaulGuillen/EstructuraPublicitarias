f3f026780cc05cf5538251cfecb8d1658f2a1cf2
Paul Guillen <pguillea@emeal.nttdata.com>
2023-09-19 12:10:23 -0500

    Initial commit

---
+++ app/src/main/java/com/devpaul/estructurapublicitarias_roal/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.devpaul.estructurapublicitarias_roal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
